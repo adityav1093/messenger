d1fc8a74150fa75d7f9b12aa2de96f7dc86a6f8d
aditya vasudeva <aditya.vasudeva@nagarro.com>
2020-06-21 02:48:28 +0530

    Updates

---
+++ precache-manifest.78b8a76da73fd1534315d4a4d5cfa27e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39fb5da64b9283342c5b2b77d849e46c",
+    "url": "/messenger/index.html"
+  },
+  {
+    "revision": "d186cd80f1195837b183",
+    "url": "/messenger/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a7e1e1897a38c18876a8",
+    "url": "/messenger/static/css/main.185f5199.chunk.css"
+  },
+  {
+    "revision": "d186cd80f1195837b183",
+    "url": "/messenger/static/js/2.91aaf2c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/messenger/static/js/2.91aaf2c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7e1e1897a38c18876a8",
+    "url": "/messenger/static/js/main.d9a6e5cc.chunk.js"
+  },
+  {
+    "revision": "ee47b741354c9eb889a3",
+    "url": "/messenger/static/js/runtime-main.6750c0e1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/messenger/static/media/logo.5d5d9eef.svg"
+  }
+]);
